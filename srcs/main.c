36a2e58e26ec7846eb653b15f368c464fcfec160
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-23 18:50:39 +0200

    Before starting to implement finding all the links in all the rooms

--- srcs/main.c
+++ srcs/main.c
@@ -3,17 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gsteyn <gsteyn@student.wethinkcode.co.z    +#+  +:+       +#+        */
+/*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 13:15:06 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/22 13:16:49 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/23 09:41:56 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
+#include "libft.h"
 
 int         main(void)
 {
+    t_list      *maplines;
+    t_list      *rooms;
+    t_list      *ants;
 
+    maplines = get_map();
+    ants = init_ants(maplines);
     return (0);
 }
