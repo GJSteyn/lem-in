b64906d5411d3041fca902458bcf00b957e49da8
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-26 12:53:43 +0200

    Fix error when checking for rooms and links

--- srcs/check_input.c
+++ srcs/check_input.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 16:39:13 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/26 12:28:56 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/26 12:53:18 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int		valid_line(char *line)
 	if (ft_strlen(line) == 0)
 		return (0);
 	if (is_comment(line) || is_room(line) || is_end(line) || is_start(line) ||
-		is_link(line) || is_command(line))
+		is_link(line) || is_command(line) || ft_is_num(line))
 		return (1);
 	return (0);
 }
@@ -51,13 +51,14 @@ int				valid_input(t_list *in)
 	if (!ft_is_num((char*)in->content))
 		ft_error("First input not number of ants");
 	in = in->next;
-	if (!in || !is_room((char*)in->content) || !is_start((char*)in->content) ||
-		!is_end((char*)in->content))
+	if (!in || (!is_room((char*)in->content) && !is_start((char*)in->content) &&
+		!is_end((char*)in->content)))
 		ft_error("Room input error right after number of ants");
 	while (in && (is_room((char*)in->content) || is_comment((char*)in->content)
 		|| is_start((char*)in->content) || is_end((char*)in->content)))
 		in = in->next;
-	if (!in || !is_comment((char*)in->content) || !is_link((char*)in->content))
+	if (!in || (!is_comment((char*)in->content) &&
+		!is_link((char*)in->content)))
 		ft_error("Link input error right after rooms");
 	while (in && (is_link((char*)in->content) ||
 		is_comment((char*)in->content)))
