9f8e10e70d92f68d1d93cb2512ea353b5b25c1c4
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-27 12:17:50 +0200

    Fix leak in get_input

--- srcs/get_input.c
+++ srcs/get_input.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 09:14:24 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/26 12:29:03 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/27 12:17:29 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ t_list			*get_input(void)
 		ft_lstappend(&ret, ft_lstnew(in, ft_strlen(in) + 1));
 		ft_strdel(&in);
 	}
+	ft_strdel(&in);
 	if (!ret)
 		ft_error("No input :(");
 	return (ret);
