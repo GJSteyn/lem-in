07d93cbd6aae1d3c19a5346178eca115824dc6c2
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-27 15:03:18 +0200

    Fix another leak in init_rooms

--- srcs/init_rooms.c
+++ srcs/init_rooms.c
@@ -6,21 +6,12 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 09:43:59 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/27 13:45:53 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/27 14:59:16 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "libft.h"
-
-static void			destroy_room(t_room *room)
-{
-	void		*name;
-
-	name = room->name;
-	ft_memdel(&name);
-	ft_memdel((void**)(&room));
-}
 
 static t_room		*new_room(size_t idx, char *nm, t_vect2 crd, t_rmflag fl)
 {
@@ -49,7 +40,7 @@ static void			add_room(t_list **dst, char *rmstr, size_t idx, t_rmflag fl)
 	newroom = new_room(idx, split[0], coords, fl);
 	tmp = ft_lstnew(newroom, sizeof(t_room));
 	ft_lstappend(dst, tmp);
-	destroy_room(newroom);
+	ft_memdel((void**)(&newroom));
 	ft_strldel(&split);
 }
 
@@ -70,9 +61,9 @@ t_list				*init_rooms(t_list *in)
 		else if (is_room((char *)in->content))
 			add_room(&ret, (char*)in->content, idx++, normal);
 		else if (is_start((char*)in->content))
-			add_room(&ret, (char*)in->next->content, idx++, start);
+			add_room(&ret, (char*)(in = in->next)->content, idx++, start);
 		else if (is_end((char*)in->content))
-			add_room(&ret, (char*)in->next->content, idx++, end);
+			add_room(&ret, (char*)(in = in->next)->content, idx++, end);
 		else if (is_link((char*)in->content))
 			break ;
 		in = in->next;
