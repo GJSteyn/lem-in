864b0c13d9f0b4268a48434c474f93b22c5db006
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-08-01 07:59:51 +0200

    Checker works

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,13 +6,12 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 05:49:53 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/31 12:11:48 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/08/01 07:55:42 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-/*
 static void		check_ant(int ant, t_list *ants)
 {
 	while (ants)
@@ -52,18 +51,9 @@ static void		check_connection(int ant, char *room, t_list *ants)
 	cnx = (t_list*)a->room->conx;
 	while (cnx)
 	{
-		ft_putstr_fd("Here\n", 2);
-		rm = (t_room*)cnx->content;
-		ft_putstr_fd("Here\n", 2);
-		if (!room)
-			ft_putstr_fd("WTF?\n", 2);
-		if (!rm)
-			ft_putstr_fd("WTF?1\n", 2);
-		ft_putstr_fd(rm->name, 2);
-		ft_putstr_fd((char*)rm->name, 2);
+		rm = *(t_room**)cnx->content;
 		if (ft_strcmp((char*)rm->name, room) == 0)
 			return ;
-		ft_putstr_fd("Doing\n", 2);
 		cnx = cnx->next;
 	}
 	ft_putstr_fd("Ant ", 2);
@@ -73,14 +63,26 @@ static void		check_connection(int ant, char *room, t_list *ants)
 	ft_error("!");
 }
 
+t_ant			*get_ant(int ant, t_list *ants)
+{
+	while (((t_ant*)ants->content)->num != (size_t)ant)
+		ants = ants->next;
+	return ((t_ant*)ants->content);
+}
+
+t_room			*get_room(char *room, t_list *rooms)
+{
+	while (ft_strcmp(((t_room*)rooms->content)->name, room) != 0)
+		rooms = rooms->next;
+	return ((t_room*)rooms->content);
+}
+
 static int		valid_move(int ant, char *room, t_list *ants, t_list *rooms)
 {
-	ft_putstr_fd("Checking ant\n", 2);
 	check_ant(ant, ants);
-	ft_putstr_fd("Checking room\n", 2);
 	check_room(ant, room, rooms);
-	ft_putstr_fd("Checking connection\n", 2);
 	check_connection(ant, room, ants);
+	move_ant(get_ant(ant, ants), get_room(room, rooms), 1);
 	return (1);
 }
 
@@ -94,8 +96,6 @@ static void		run_instructions(t_list *instr, t_list *rooms, t_list *ants)
 	i = 0;
 	while (instr)
 	{
-		ft_putnbr_fd(i++, 2);
-		ft_putchar_fd(' ', 2);
 		if (!is_instruction((char*)instr->content))
 		{
 			ft_putstr_fd("Instruction: ", 2);
@@ -107,42 +107,40 @@ static void		run_instructions(t_list *instr, t_list *rooms, t_list *ants)
 		room = split[1];
 		if (valid_move(ant, room, ants, rooms))
 		{
-			ft_putstr_fd("Got\n", 2);
 			ft_strldel(&split);
 			instr = instr->next;
 		}
 	}
-	ft_putstr_fd("End of instructions\n", 2);
 }
-*/
+
+void			check_ants(t_list *ants)
+{
+	while (ants)
+	{
+		if (((t_ant*)ants->content)->room->type != end)
+		{
+			ft_putstr_fd("Ant ", 2);
+			ft_putnbr_fd(((t_ant*)ants->content)->num, 2);
+			ft_error(" has sadly been left behind");
+		}
+		ants = ants->next;
+	}
+}
 
 int				main(void)
 {
 	t_list			*input;
 	t_list			*rooms;
 	t_list			*ants;
-	//t_list			*instr;
+	t_list			*instr;
 
 	input = get_input();
 	rooms = init_rooms(input);
 	get_links(rooms, input);
 	ants = init_ants(input, rooms);
-	/*
-	t_room			*room;
-	t_list			*cnx;
-	while (rooms)
-	{
-		room = (t_room*)rooms->content;
-		cnx = room->conx;
-		while (cnx)
-		{
-			room = (t_room*)cnx->content;
-			ft_putendl_fd(room->name, 2);
-			cnx = cnx->next;
-		}
-		rooms = rooms->next;
-	}
-	*/
-	//run_instructions(instr, rooms, ants);
+	instr = get_instructions(input);
+	run_instructions(instr, rooms, ants);
+	check_ants(ants);
+	ft_putstr_fd("All ants have successfully navigated to the end!\n", 2);
 	return (0);
 }
